b0cd130980a8cdf56805d31ddd31b7d1dfc7cdab
John Hershberg <johnher@gmail.com>
2020-08-15 23:09:04 -0700

    Textures are now working

--- src/3d/PlanetMaterial.tsx
+++ src/3d/PlanetMaterial.tsx
@@ -7,18 +7,21 @@ interface PlanetMaterialProps {
 
 export default function PlanetMaterial({ diffuse }: PlanetMaterialProps) {
   const vertexShader = `
+                        varying vec2 vUv;
                         void main() {
+                          vUv = uv;
                           gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
                         }`;
 
   const fragmentShader = `
+                        varying vec2 vUv;
                         uniform vec4 color;
                         uniform sampler2D tDiffuse;
 
                         void main() {
-                          vec2 uv = gl_FragCoord.xy;
+                          vec2 uv = vUv;
                           vec4 tex = texture2D(tDiffuse, uv);
-                          gl_FragColor = vec4( tex  );
+                          gl_FragColor = vec4( tex * color );
                         }`;
 
   const data = useMemo(
