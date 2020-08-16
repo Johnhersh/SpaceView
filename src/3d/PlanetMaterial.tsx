6999d968021831e98afba42292ce3cd187a7ae39
John Hershberg <johnher@gmail.com>
2020-08-15 21:11:10 -0700

    Moved shaders to consts

--- src/3d/PlanetMaterial.tsx
+++ src/3d/PlanetMaterial.tsx
@@ -1,28 +1,34 @@
 import React, { useMemo } from "react";
 
 export default function PlanetMaterial() {
-  const uniforms = useMemo((): any => {
-    return {
-      resolution: "512",
-      color: "0.1, 0.0, 0.0, 1.0",
-    };
-  }, []);
+  const vertexShader = `
+                        void main() {
+                          gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+                        }`;
+
+  const fragmentShader = `
+                        void main() {
+                          gl_FragColor = vec4( 0.0, 0.0, 1.0, 1.0 );
+                        }`;
+
+  const data = useMemo(
+    () => ({
+      uniforms: {},
+      // fragmentShader,
+      // vertexShader,
+    }),
+    []
+  );
 
   return (
     <shaderMaterial
       attach="material"
+      {...data}
       args={[
         {
-          uniforms,
-          vertexShader: `
-                        void main() {
-                          gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
-                        }`,
-          fragmentShader: `
-                        void main() {
-                          gl_FragColor = vec4( 0.0, 1.0, 0.0, 1.0 );
-                        }
-                        `,
+          // uniforms,
+          vertexShader: vertexShader,
+          fragmentShader: fragmentShader,
         },
       ]}
     />
