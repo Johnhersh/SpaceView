74de123ec6c4d2ead4c55299ae74f0c5adbb6f2b
John Hershberg <johnher@gmail.com>
2020-08-17 23:19:13 -0700

    Added bootstrap and slider

---
+++ src/custom_typings/react-bootstrap-range-slider.d.ts
@@ -0,0 +1,4 @@
+declare module "react-bootstrap-range-slider" {
+  const value: any;
+  export default value;
+}
--- src/pages/homepage.component.tsx
+++ src/pages/homepage.component.tsx
@@ -1,7 +1,10 @@
 import React, { Suspense } from "react";
+import "./homepage.styles.scss";
 import { Canvas } from "react-three-fiber";
 import { Vector3 } from "three";
 import { softShadows, OrbitControls, Html } from "drei";
+import RangeSlider from "react-bootstrap-range-slider";
+
 import PlanetMesh from "../3d/PlanetMesh";
 
 softShadows({
@@ -14,37 +17,45 @@ softShadows({
 
 function HomePage() {
   return (
-    <>
-      <Canvas colorManagement camera={{ position: [-5, 2, 10], fov: 60 }} shadowMap>
-        <ambientLight intensity={0.3} />
-        <directionalLight
-          castShadow
-          position={[0, 10, 0]}
-          intensity={1.5}
-          shadow-mapSize-width={2048}
-          shadow-mapSize-height={2048}
-          shadow-camera-far={50}
-          shadow-camera-left={-10}
-          shadow-camera-right={10}
-          shadow-camera-top={10}
-          shadow-camera-bottom={-10}
-        />
-        <pointLight position={[-10, 0, -20]} intensity={0.5} />
-        <pointLight position={[0, -10, 0]} intensity={1.5} />
-        <group>
-          <mesh receiveShadow rotation={[-Math.PI / 2, 0, 0]} position={[0, -5, 35]}>
-            <planeBufferGeometry attach="geometry" args={[100, 100]} />
-            <shadowMaterial attach="material" opacity={0.3} />
-          </mesh>
-        </group>
-        <Suspense fallback={<Html>Loading</Html>}>
-          <PlanetMesh position={new Vector3(0, 1, 0)} />
-          <PlanetMesh position={new Vector3(-2, 1, -5)} />
-          <PlanetMesh position={new Vector3(5, 1, -2)} />
-        </Suspense>
-        <OrbitControls />
-      </Canvas>
-    </>
+    <div className="homepageContainer">
+      <div className="canvasContainer">
+        <Canvas colorManagement camera={{ position: [-5, 2, 10], fov: 60 }} shadowMap>
+          <ambientLight intensity={0.3} />
+          <directionalLight
+            castShadow
+            position={[0, 10, 0]}
+            intensity={1.5}
+            shadow-mapSize-width={2048}
+            shadow-mapSize-height={2048}
+            shadow-camera-far={50}
+            shadow-camera-left={-10}
+            shadow-camera-right={10}
+            shadow-camera-top={10}
+            shadow-camera-bottom={-10}
+          />
+          <pointLight position={[-10, 0, -20]} intensity={0.5} />
+          <pointLight position={[0, -10, 0]} intensity={1.5} />
+          <group>
+            <mesh receiveShadow rotation={[-Math.PI / 2, 0, 0]} position={[0, -5, 35]}>
+              <planeBufferGeometry attach="geometry" args={[100, 100]} />
+              <shadowMaterial attach="material" opacity={0.3} />
+            </mesh>
+          </group>
+          <Suspense fallback={<Html>Loading</Html>}>
+            <PlanetMesh position={new Vector3(0, 1, 0)} />
+            <PlanetMesh position={new Vector3(-2, 1, -5)} />
+            <PlanetMesh position={new Vector3(5, 1, -2)} />
+          </Suspense>
+          <OrbitControls />
+        </Canvas>
+      </div>
+      <div className="settingsContainer">
+        <h1>JOHN HERSHBERG</h1>
+        <div className="sliderContainer">
+          <RangeSlider value={10} />
+        </div>
+      </div>
+    </div>
   );
 }
 
