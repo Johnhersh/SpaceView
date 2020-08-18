b5ecaedd05ef5cecdbfd24018e1afaae0f044bcd
John Hershberg <johnher@gmail.com>
2020-08-17 19:22:54 -0700

    Dissolve shader works

--- src/3d/PlanetMaterial.tsx
+++ src/3d/PlanetMaterial.tsx
@@ -1,40 +1,53 @@
 import React, { useMemo } from "react";
-import { Vector4, Texture } from "three";
+import { Vector4, Texture, NormalBlending } from "three";
+
+const vertexShader = `
+varying vec2 vUv;
+void main() {
+  vUv = uv;
+  gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+}
+`;
+
+const fragmentShader = `
+varying vec2 vUv;
+uniform vec4 color;
+uniform sampler2D tDiffuse;
+uniform float dissolveAmount;
+
+void main() {
+    vec2 uv = vUv;
+    float softness = 0.1;
+    float scaleAndOffset = (dissolveAmount * softness) + dissolveAmount;
+    float minValue = scaleAndOffset - softness;
+    float maxValue = scaleAndOffset;
+    vec4 tex = texture2D(tDiffuse, uv);
+    float dissolve = smoothstep(minValue, maxValue, tex.r);
+    vec4 finalColor = color * tex;
+    finalColor.a = dissolve;
+    gl_FragColor = vec4( finalColor );
+}
+`;
 
 interface PlanetMaterialProps {
   diffuse: Texture;
 }
 
 export default function PlanetMaterial({ diffuse }: PlanetMaterialProps) {
-  const vertexShader = `
-                        varying vec2 vUv;
-                        void main() {
-                          vUv = uv;
-                          gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
-                        }`;
-
-  const fragmentShader = `
-                        varying vec2 vUv;
-                        uniform vec4 color;
-                        uniform sampler2D tDiffuse;
-
-                        void main() {
-                          vec2 uv = vUv;
-                          vec4 tex = texture2D(tDiffuse, uv);
-                          gl_FragColor = vec4( tex * color );
-                        }`;
-
   const data = useMemo(
     () => ({
       uniforms: {
         color: { value: new Vector4(0.0, 0.0, 1.0, 1.0) },
         tDiffuse: { value: diffuse },
+        dissolveAmount: { value: 0.5 },
       },
       fragmentShader,
       vertexShader,
     }),
-    [vertexShader, fragmentShader, diffuse]
+    [diffuse]
   );
 
-  return <shaderMaterial attach="material" {...data} />;
+  return (
+    <shaderMaterial blending={NormalBlending} transparent={true} attach="material" {...data} />
+  );
 }
