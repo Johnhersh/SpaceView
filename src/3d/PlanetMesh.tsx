9d9ed1aefa3f2b7175dbd9a167a28d512c4f5ba1
John Hershberg <johnher@gmail.com>
2020-08-26 20:41:34 -0700

    Rings now respect alpha

--- src/3d/PlanetMesh.tsx
+++ src/3d/PlanetMesh.tsx
@@ -5,6 +5,7 @@ import { useFrame, useLoader } from "react-three-fiber";
 import PlanetMaterial from "./PlanetMaterial";
 
 import ringsDefaultTexture from "./textures/Rings_Default.png";
+import ringsDefaultAlphaTexture from "./textures/Rings_Default_Alpha.png";
 
 interface SpinningMeshProps {
   position: THREE.Vector3;
@@ -26,6 +27,7 @@ const PlanetMesh = ({
   const mesh = useRef<THREE.Mesh>();
   const texture = useLoader(THREE.TextureLoader, texturePath);
   const ringsTexture = useLoader(THREE.TextureLoader, ringsDefaultTexture);
+  const ringsTextureAlpha = useLoader(THREE.TextureLoader, ringsDefaultAlphaTexture);
   if (ringsTexture) {
     if (ringsTexturePath) {
     }
@@ -53,7 +55,13 @@ const PlanetMesh = ({
       {rings && (
         <mesh position={position}>
           <cylinderBufferGeometry attach="geometry" args={[1, ringsRadius, 0.01, 64, 12, true]} />
-          <meshBasicMaterial attach="material" map={ringsTexture} side={THREE.DoubleSide} />
+          <meshBasicMaterial
+            attach="material"
+            map={ringsTexture}
+            transparent={true}
+            alphaMap={ringsTextureAlpha}
+            side={THREE.DoubleSide}
+          />
         </mesh>
       )}
     </group>
